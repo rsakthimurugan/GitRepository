790e05a97a964f2bafa6fee6bb070e3f2744b209
rsakt <rsakt@DESKTOP-Q608TF9>
2019-10-10 00:25:46 +0200

    initial version

---
+++ Hello World/src/FirstPackage/FirstClass.java
@@ -0,0 +1,12 @@
+package FirstPackage;
+
+public class FirstClass {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hello this is first project");
+
+	}
+
+}
